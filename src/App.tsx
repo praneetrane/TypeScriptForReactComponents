83c71fbcf5baaa8e2ee0d60572102a1ae8f7f2bf
Praneet Rane <praneet.rane@gmail.com>
2022-10-29 21:12:40 +0530

    TS patterns & practices for React Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement, ReactNode } from "react";
+import "./App.css";
+
+//conventional props
+function Heading({ title }: { title: string }) {
+  return <h1>{title}</h1>;
+}
+
+function HeadingWithContent({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement {
+  return <h1>{children}</h1>;
+}
+
+//Default Props
+const defaultContainerProps = { heading: <strong>My Heading</strong> };
+
+type ContainerProps = { children: ReactNode } & typeof defaultContainerProps; //If we dont provide header it will automatically have default header.
+function Container({ heading, children }: ContainerProps) {
+  return (
+    <div>
+      <h1>{heading}</h1>
+      {children}
+    </div>
+  );
+}
+Container.defaultProps = defaultContainerProps;
+
+//Functional props
+function TextWithNumber({
+  header,
+  children,
+}: {
+  header?: (num: number) => ReactNode;
+  children: (num: number) => ReactNode;
+}) {
+  const [state, setState] = React.useState<number>(0);
+
+  return (
+    <div>
+      {header && <h2>{header?.(state)}</h2>}
+      <div>{children(state)}</div>
+      <div>
+        <button onClick={() => setState(state + 1)}>Add</button>
+      </div>
+    </div>
+  );
+}
+
+//List using concept of Generics. Created generic List function
+function List<ListItem>({
+  items,
+  render,
+}: {
+  items: ListItem[];
+  render: (item: ListItem) => ReactNode;
+}) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
+//Class component
+class MyHeader extends React.Component<{
+  title: ReactNode;
+}> {
+  render() {
+    return <h1>{this.props.title}</h1>;
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Hello"></Heading>
+      <HeadingWithContent>
+        <strong>Hi!</strong>
+      </HeadingWithContent>
+      <Container>Foo</Container>
+      <TextWithNumber header={(num: number) => <span>Header {num}</span>}>
+        {(num: number) => <div>Today's number is num {num}</div>}
+      </TextWithNumber>
+      <List
+        items={["Praneet", "Shambhavi", "Amruta"]}
+        render={(item: string) => <div>{item.toLowerCase()}</div>}
+      ></List>
+      <MyHeader title="Hello there!"></MyHeader>
     </div>
   );
 }
